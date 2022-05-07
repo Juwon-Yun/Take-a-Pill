c5d1152bb7cdf6922e42ce48ba5b408a5e8b45a3
Juwon-Yun <eooll2@daum.net>
2022-05-07 23:28:12 +0900

    feat: add alarm page simple ui

--- lib/pages/add_medicine/add_alarm_page.dart
+++ lib/pages/add_medicine/add_alarm_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/custom_constants.dart';
 
@@ -23,11 +24,56 @@ class AddAlarmPage extends StatelessWidget {
         const SizedBox(height: largeSpace),
           // 기존에 SingleChildScrollView를 해놓아서 나머지 높이가 무제한 Expanded 되기 때문에 에러를 표시한다.
           Expanded(child: ListView(
-            children: [
+            children: const [
+              AlarmBox(),
+              AlarmBox(),
+              AlarmBox(),
+              AlarmBox(),
+              AddAlarmButton(),
             ],
           ))
         ],
         
+      ),
+    );
+  }
+}
+
+class AlarmBox extends StatelessWidget {
+  const AlarmBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6), textStyle: Theme.of(context).textTheme.subtitle2),
+      onPressed: (){},
+      child: Row(
+        children: [
+          Expanded(flex: 1, child: IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.minus_circled))),
+          Expanded(flex: 5, child: TextButton(onPressed: (){}, child: const Text('18:00'))),
+        ],
+      ),
+    );
+  }
+}
+
+class AddAlarmButton extends StatelessWidget {
+  const AddAlarmButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6), textStyle: Theme.of(context).textTheme.subtitle1),
+      onPressed: (){},
+      child: Row(
+        children: const [
+          Expanded(flex: 1, child: Icon(CupertinoIcons.plus_circled)),
+          Expanded(flex: 5, child: Center(child:Text('복용 시간 추가')) ,),
+        ],
       ),
     );
   }
